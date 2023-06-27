fcd1ce82d936ddb5ef1003b470c280317ad4d654
stevendepauw <88113901+stevendepauw@users.noreply.github.com>
2023-06-26 22:30:15 -0500

    fixed lint errors

--- src/components/NewListing.tsx
+++ src/components/NewListing.tsx
@@ -1,7 +1,7 @@
 import { useSession } from "next-auth/react";
 import { CustomButton } from "./CustomButton";
 import { ProfileImage } from "./ProfileImage";
-import { FormEvent, useCallback, useLayoutEffect, useRef, useState } from "react";
+import { type FormEvent, useCallback, useLayoutEffect, useRef, useState } from "react";
 import { api } from "~/utils/api";
 
 function updatePostSize(textArea?: HTMLTextAreaElement) {
