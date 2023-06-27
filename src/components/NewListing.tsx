d9f69382b5e83df63c3bffc50ebd2d7e35ba0feb
stevendepauw <88113901+stevendepauw@users.noreply.github.com>
2023-06-27 13:22:33 -0500

    Odr steven 1 (#5)
    * Base layout for making a post complete
    
    * should have fixed issue preventing the successfull deployment with the current code
    
    * base UI fr homepage and creating new posts created and layed out, can create posts onto the server
    
    * fixed lint errors
    
    * removed [id] page that was throwing an error on vercel deployment for now

---
+++ src/components/NewListing.tsx
@@ -0,0 +1,66 @@
+import { useSession } from "next-auth/react";
+import { CustomButton } from "./CustomButton";
+import { ProfileImage } from "./ProfileImage";
+import { type FormEvent, useCallback, useLayoutEffect, useRef, useState } from "react";
+import { api } from "~/utils/api";
+
+function updatePostSize(textArea?: HTMLTextAreaElement) {
+    if(textArea == null) return
+    textArea.style.height = '0'
+    textArea.style.height = `${textArea.scrollHeight}px`
+}
+
+export function NewListing() {
+    const session = useSession();
+    if (session.status !== "authenticated") return
+
+    return <Form />
+}
+
+function Form() {
+    const session = useSession();
+  const [inputValue, setInputValue] = useState("");
+  const textAreaRef = useRef<HTMLTextAreaElement>()
+  const inputRef = useCallback((textArea: HTMLTextAreaElement) => {
+    updatePostSize(textArea);
+    textAreaRef.current = textArea
+  }, [])
+
+  useLayoutEffect(() => {
+    updatePostSize(textAreaRef.current);
+  }, [inputValue]);
+
+  const createPost = api.post.create.useMutation({
+    onSuccess: (newPost) => {
+      console.log(newPost);
+      setInputValue("");
+    }
+  })
+
+  if (session.status !== "authenticated") return null;
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault()
+
+    createPost.mutate({ content: inputValue })
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col gap-2 border-b px-4 py-2">
+      <div className="flex gap-4">
+        <ProfileImage src={session.data.user.image} />
+        <textarea
+            ref={inputRef}
+          style={{ height: 0 }}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          className="p4 flex-grow resize-none overflow-hidden text-lg outline-none"
+          placeholder="Tell us about it"
+        />
+      </div>
+      <CustomButton className="self-end">Post</CustomButton>
+    </form>
+  );
+}
+
+
