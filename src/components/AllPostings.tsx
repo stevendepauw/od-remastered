6eb5ba8e56bd77e742c65d82f70355f38992d880
stevendepauw <88113901+stevendepauw@users.noreply.github.com>
2023-06-28 09:50:13 -0500

    Can now pull in the recent postings, edited side bar to include icons, added like button with some stying

--- src/components/AllPostings.tsx
+++ src/components/AllPostings.tsx
@@ -1,4 +1,9 @@
 import InfiniteScroll from "react-infinite-scroll-component";
+import { ProfileImage } from "./ProfileImage";
+import Link from "next/link";
+import { useSession } from "next-auth/react";
+import { VscHeartFilled, VscHeart } from "react-icons/vsc";
+import { IconHoverEffects } from "./IconHoverEffects";
 
 type Post = {
   id: string;
@@ -43,9 +48,86 @@ export function AllPostings({
         loader={"Loading..."}
       >
         {posts.map((post) => {
-          return <div key={post.id}>{post.content}</div>;
+          return <PostCard key={post.id} {...post} />;
         })}
       </InfiniteScroll>
     </ul>
+  );
+}
+
+const dateTimeFormatter = new Intl.DateTimeFormat(undefined, {
+  dateStyle: "short",
+});
+
+function PostCard({
+  id,
+  user,
+  content,
+  createdAt,
+  likeCount,
+  likedByMe,
+}: Post) {
+  return (
+    <li className="flex gap-4 border-b px-4 py-4">
+      <Link href={`/profiles/${user.id}`}>
+        <ProfileImage src={user.image} />
+      </Link>
+      <div className="flex flex-grow flex-col">
+        <div className="flex gap-1">
+          <Link
+            href={`/profiles/${user.id}`}
+            className="focus-visible:underine font-bold outline-none hover:underline"
+          >
+            {user.name}
+          </Link>
+          <span className="text-gray-500">-</span>
+          <span className="text-gray-500">
+            {dateTimeFormatter.format(createdAt)}
+          </span>
+        </div>
+        <p className="whitespace-pre-wrap">{content}</p>
+        <HeartButton likedByMe={likedByMe} likeCount={likeCount} />
+      </div>
+    </li>
+  );
+}
+
+type HeartButtonProps = {
+  likedByMe: boolean;
+  likeCount: number;
+};
+
+function HeartButton({ likedByMe, likeCount }: HeartButtonProps) {
+  const session = useSession();
+  const HeartIcon = likedByMe ? VscHeartFilled : VscHeart;
+
+  if (session.status !== "authenticated") {
+    return (
+      <div className="mb-1 mt-1 flex items-center gap-3 self-start text-gray-500">
+        <HeartIcon />
+        <span>{likeCount}</span>
+      </div>
+    );
+  }
+  return (
+    <button
+      className={`group flex items-center gap-1 self-start transition-colors duration-200 -ml-2 ${
+        likedByMe
+          ? "text-red-500"
+          : "text-gray-500 hover:text-red-500 focus-visible:text-red-500"
+      }`}
+    >
+      <IconHoverEffects red>
+      <HeartIcon
+        className={`transition-colors duration-200 ${
+          likedByMe
+            ? "fill-red-500"
+            : "fill-gray-500 group-hover:fill-red-500 group-focus-visible:fill-red-500"
+        }`}
+      />
+      </IconHoverEffects>
+      
+      <span>{likeCount}</span>
+    </button>
   );
 }
