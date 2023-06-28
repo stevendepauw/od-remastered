082de88267c57aff6a57340529755b283180d4e6
stevendepauw <88113901+stevendepauw@users.noreply.github.com>
2023-06-27 23:48:42 -0500

    why didnt it push these the first time i said commit

--- src/components/NewListing.tsx
+++ src/components/NewListing.tsx
@@ -1,30 +1,36 @@
 import { useSession } from "next-auth/react";
 import { CustomButton } from "./CustomButton";
 import { ProfileImage } from "./ProfileImage";
-import { type FormEvent, useCallback, useLayoutEffect, useRef, useState } from "react";
+import {
+  type FormEvent,
+  useCallback,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
 import { api } from "~/utils/api";
 
 function updatePostSize(textArea?: HTMLTextAreaElement) {
-    if(textArea == null) return
-    textArea.style.height = '0'
-    textArea.style.height = `${textArea.scrollHeight}px`
+  if (textArea == null) return;
+  textArea.style.height = "0";
+  textArea.style.height = `${textArea.scrollHeight}px`;
 }
 
 export function NewListing() {
-    const session = useSession();
-    if (session.status !== "authenticated") return null
+  const session = useSession();
+  if (session.status !== "authenticated") return null;
 
-    return <Form />
+  return <Form />;
 }
 
 function Form() {
-    const session = useSession();
+  const session = useSession();
   const [inputValue, setInputValue] = useState("");
-  const textAreaRef = useRef<HTMLTextAreaElement>()
+  const textAreaRef = useRef<HTMLTextAreaElement>();
   const inputRef = useCallback((textArea: HTMLTextAreaElement) => {
     updatePostSize(textArea);
-    textAreaRef.current = textArea
-  }, [])
+    textAreaRef.current = textArea;
+  }, []);
 
   useLayoutEffect(() => {
     updatePostSize(textAreaRef.current);
@@ -33,24 +39,27 @@ function Form() {
   const createPost = api.post.create.useMutation({
     onSuccess: (newPost) => {
       setInputValue("");
-      console.log(newPost)
-    }
-  })
+      console.log(newPost);
+    },
+  });
 
   if (session.status !== "authenticated") return null;
 
   function handleSubmit(e: FormEvent) {
-    e.preventDefault()
+    e.preventDefault();
 
-    createPost.mutate({ content: inputValue })
+    createPost.mutate({ content: inputValue });
   }
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col gap-2 border-b px-4 py-2">
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-2 border-b px-4 py-2"
+    >
       <div className="flex gap-4">
         <ProfileImage src={session.data.user.image} />
         <textarea
-            ref={inputRef}
+          ref={inputRef}
           style={{ height: 0 }}
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
@@ -62,5 +71,3 @@ function Form() {
     </form>
   );
 }
-
-
