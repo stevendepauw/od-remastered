b88becec618b7c3a54560f4518aa90dad0ca2d3c
alexbrenes999 <116224156+alexbrenes999@users.noreply.github.com>
2023-06-28 12:27:56 -0400

    almost done with the profile setup

--- src/pages/profiles/[id].tsx
+++ src/pages/profiles/[id].tsx
@@ -4,24 +4,62 @@ import { ssgHelper } from "~/server/ssgHelper";
 import { useSession } from "next-auth/react";
 import { api } from "~/utils/api"
 import ErorrPage from 'next/error'
+import Link from "next/link";
+import { ProfileImage } from "~/components/ProfileImage";
+import { AllPostings } from "~/components/AllPostings";
 
 const ProfilePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ id, }) => {
     const { data: profile} = api.profile.getById.useQuery({ id })
-    if (profile == null) return <ErorrPage statusCode={404}/>
+    if (profile == null || profile.name == null) return <ErorrPage statusCode={404}/>
     return <>
         <Head>
             <title>{`Only Devs - ${profile.name}`}</title>
         </Head>
-        {profile.name}
+        <header>
+            <Link href=".." className="mr-2">
+                {/* <IconHoverEffect>
+                    <VscArrowLeft className="h-6 w-6"/>
+                </IconHoverEffect> */}
+            </Link>
+            <ProfileImage src={profile.image} className="flex-shrink-0"/>
+            <div className="ml-2 flex-grow">
+                <h1 className="text-lg font-bold">{profile.name}</h1>
+                <div className="text-gray-500">
+                    {profile.postsCount}{" "}
+                    {getPlural(profile.postsCount, "Post", "Posts")} -{" "}
+
+                    {profile.followersCount}{" "}
+                    {getPlural(profile.followersCount, "Follower", "Followers")} -{" "}
+
+                    {profile.followsCount} Following
+
+                </div>
+                {/* <FollowButton isFollowing={profile.isFollowing} userId={ id }
+                    onClick = {() => null}/> */}
+            </div>
+            <main>
+                {/* <AllPostings /> */}
+            </main>
+        </header>
     </>
 };
+
+function FollowButton() {
+    return <h1>Follow</h1>
+}
+
+const pluralRules = new Intl.PluralRules();
+
+function getPlural(number: number, singular: string, plural: string) {
+    return pluralRules.select(number) === "one" ? singular : plural
+}
 
 export const getStaticPaths: GetStaticPaths = () => {
     return {
         paths: [],
         fallback: "blocking"
     }
-}
+};
 
 export async function getStaticProps(context:GetStaticPropsContext <{ id: string}>
     ) {
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,54 +1,57 @@
+import { Prisma } from "@prisma/client";
+import { inferAsyncReturnType } from "@trpc/server";
 import { z } from "zod";
+
 import {
   createTRPCRouter,
   publicProcedure,
   protectedProcedure,
+  createTRPCContext,
 } from "~/server/api/trpc";
 
 export const postRouter = createTRPCRouter({
-  allPostsFeed: publicProcedure.input(
+  allProfileFeed:  publicProcedure
+  .input(
     z.object({ 
+      userId: z.string(),
       limit: z.number().optional(), 
       cursor: z.object({ id: z.string(), createdAt: z.date()}).optional(),
     })
-  ).query(async ({ input: { limit = 10, cursor }, ctx }) => {
-    const currentUserId = ctx.session?.user.id
-
-    const data =  await ctx.prisma.post.findMany({
-      take: limit + 1,
-      cursor: cursor ? { createdAt_id: cursor } : undefined,
-      orderBy: [{ createdAt: "desc"}, { id: "desc" }],
-      select: {
-        id:true,
-        content: true,
-        createdAt:true,
-        _count: { select: { likes: true }},
-        likes: currentUserId == null ? false : { where: { userId: currentUserId }},
-        user: {
-          select: { name: true, id: true, image: true }
-        }
+  )
+  .query(async ({ input: { limit = 10, userId, cursor }, ctx }) => {
+      return await getAllPosts({
+        limit,
+        ctx,
+        cursor,
+        whereClause: { userId },
+      });
+    }),
+    allPostsFeed: publicProcedure
+    .input(
+      z.object({
+        onlyFollowing: z.boolean().optional(),
+        limit: z.number().optional(),
+        cursor: z.object({ id: z.string(), createAt: z.date() }).optional(),
+      })
+    )
+    .query(
+      async ({ input: { limit = 10, onlyFollowing = false, cursor }, ctx }) => {
+        const currentUserId = ctx.session?.user.id;
+        return await getAllPosts({
+          limit,
+          ctx,
+          cursor,
+          whereClause:
+            currentUserId == null || !onlyFollowing
+              ? undefined
+              : {
+                  user: {
+                    followers: { some: { id: currentUserId } },
+                  },
+                },
+        });
       }
-    });
-
-    let nextCursor: typeof cursor | undefined
-    if(data.length > limit) {
-      const nextItem = data.pop()
-      if (nextItem != null) {
-        nextCursor = { id: nextItem.id, createdAt: nextItem.createdAt}
-      }
-    }
-
-    return { posts: data.map(post => {
-      return {
-        id: post.id,
-        content: post.content,
-        createdAt: post.createdAt,
-        likeCount: post._count.likes,
-        user: post.user,
-        likedByMe: post.likes?.length > 0,
-      }
-    }), nextCursor }
-  }),
+    ),
   create: protectedProcedure
     .input(z.object({ content: z.string() }))
     .mutation(async ({ input: { content }, ctx }) => {
@@ -56,6 +59,154 @@ export const postRouter = createTRPCRouter({
         data: { content, userId: ctx.session.user.id },
       });
 
-      return post
+      return post;
+    }),
+  toggleLike: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ input: { id }, ctx }) => {
+      const data = { postId: id, userId: ctx.session.user.id };
+
+      const existingLike = await ctx.prisma.like.findUnique({
+        where: { userId_postId: data },
+      });
+
+      if (existingLike == null) {
+        await ctx.prisma.like.create({ data });
+        return { addedLike: true };
+      } else {
+        await ctx.prisma.like.delete({ where: { userId_postId: data } });
+        return { addedLike: false };
+      }
     }),
 });
+
+async function getAllPosts({
+  whereClause,
+  ctx,
+  limit,
+  cursor,
+}: {
+  whereClause?: Prisma.PostWhereInput;
+  limit: number;
+  cursor: { id: string; createdAt: Date } | undefined;
+  ctx: inferAsyncReturnType<typeof createTRPCContext>;
+}) {
+  const currentUserId = ctx.session?.user.id;
+
+  const data = await ctx.prisma.post.findMany({
+    take: limit + 1,
+    cursor: cursor ? { createdAt_id: cursor } : undefined,
+    orderBy: [{ createdAt: "desc" }, { id: "desc" }],
+    where: whereClause,
+    select: {
+      id: true,
+      content: true,
+      createdAt: true,
+      _count: { select: { likes: true } },
+      likes:
+        currentUserId == null ? false : { where: { userId: currentUserId } },
+      user: {
+        select: { name: true, id: true, image: true },
+      },
+    },
+  });
+
+  let nextCursor: typeof cursor | undefined;
+  if (data.length > limit) {
+    const nextItem = data.pop();
+    if (nextItem != null) {
+      nextCursor = { id: nextItem.id, createdAt: nextItem.createdAt };
+    }
+  }
+
+  return {
+    posts: data.map((post) => {
+      return {
+        id: post.id,
+        content: post.content,
+        createdAt: post.createdAt,
+        likeCount: post._count.likes,
+        user: post.user,
+        likedByMe: post.likes?.length > 0,
+      };
+    }),
+    nextCursor,
+  };
+}
+
+  // allPostsFeed: publicProcedure
+  // .input(
+  //   z.object({ 
+  //     onlyFollowing: z.boolean().optional(),
+  //     limit: z.number().optional(), 
+  //     cursor: z.object({ id: z.string(), createdAt: z.date()}).optional(),
+  //   })
+  // )
+  // .query(async ({ input: { limit = 10, cursor }, ctx }) => {
+  //   const currentUserId = ctx.session?.user.id
+
+  //   const data =  await ctx.prisma.post.findMany({
+  //     take: limit + 1,
+  //     cursor: cursor ? { createdAt_id: cursor } : undefined,
+  //     orderBy: [{ createdAt: "desc"}, { id: "desc" }],
+  //     select: {
+  //       id:true,
+  //       content: true,
+  //       createdAt:true,
+  //       _count: { select: { likes: true }},
+  //       likes: currentUserId == null ? false : { where: { userId: currentUserId }},
+  //       user: {
+  //         select: { name: true, id: true, image: true }
+  //       }
+  //     }
+  //   });
+
+  //   let nextCursor: typeof cursor | undefined
+  //   if(data.length > limit) {
+  //     const nextItem = data.pop()
+  //     if (nextItem != null) {
+  //       nextCursor = { id: nextItem.id, createdAt: nextItem.createdAt}
+  //     }
+  //   }
+
+  //   return { posts: data.map(post => {
+  //     return {
+  //       id: post.id,
+  //       content: post.content,
+  //       createdAt: post.createdAt,
+  //       likeCount: post._count.likes,
+  //       user: post.user,
+  //       likedByMe: post.likes?.length > 0,
+  //     }
+  //   }), nextCursor }
+  // });
+
+  // create: protectedProcedure
+  //   .input(z.object({ content: z.string() }))
+  //   .mutation(async ({ input: { content }, ctx }) => {
+  //     const post = await ctx.prisma.post.create({
+  //       data: { content, userId: ctx.session.user.id },
+  //     });
+
+  //     return post
+  //   });
+
+  // toggleLike: protectedProcedure
+  //   .input(z.object({ id: z.string() }))
+  //   .mutation(async ({ input: { id }, ctx }) => {
+
+  //   const data = { postId: id, userId: ctx.session.user.id }
+
+  //   const existingLike = await ctx.prisma.like.findUnique({
+  //     where: {userId_postId: data }
+  //   });
+
+  //   if (existingLike == null) {
+  //     await ctx.prisma.like.create({ data });
+  //     return { addedLike: true };
+  //   } else {
+  //     await ctx.prisma.like.delete({ where: { userId_postId: data }});
+  //     return { addedLike: false };
+
+  //   }
+  // })
